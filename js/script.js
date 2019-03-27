61f9efc5235fc3b82417b3e8e9706fcc5fd6c655
Isaac <isaac.chang5@gmail.com>
2019-03-27 08:46:36 -0700

    styles

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  // Get the current year for the copyright
+  $('#year').text(new Date().getFullYear());
+
+  // Init Scrollspy
+  $('body').scrollspy({ target: '#main-nav' });
+
+  // Smooth Scrolling
+  $('#main-nav a').click(function(e) {
+    e.preventDefault();
+
+    var targetElement = $(this).attr('href');
+    var targetPosition = $(targetElement).offset().top;
+    $('html, body').animate({ scrollTop: targetPosition - 50 }, 'slow');
+  });
+});
